8544836062a65458db57d734eecdb4940a3d9886
Thalles Bueno <thalles.bueno@softdesign.com.br>
2019-06-14 19:34:22 -0300

    sla

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Row, Form, Button, Col } from "react-bootstrap";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className='p-0 d-flex h-100 align-items-center justify-content-center '>
+      
+      <Container col={6} className='p-0 m-0 rounded-lg bg-white'>
+
+        <Row className=' m-5'>
+          <h3>LOGIN</h3>
+        </Row>
+
+        <Form className='mx-5 mt-4 '>
+          <Form.Control lg type='email' placeholder='email' className='mb-4 form-control-lg'></Form.Control>
+          <Form.Control type='password' placeholder='password' className='mb-4 form-control-lg'></Form.Control>
+        </Form>
+
+        <Row className='justify-content-center mx-5 mb-5'>
+          <small  className='text-muted'>FORGOT YOUR PASSWORD</small>
+        </Row>
+
+
+        <Row className='mx-0 mt-4'>
+          <Col className='p-4 m-0 bg-secondary'  >
+            <a  className=''>REGISTER</a>
+          </Col>
+          <Col className='p-4 m-0 bg-dark text-white text-right'>
+            <a>SIGN IN</a>
+          </Col>
+        </Row>
+
+      </Container>
+
+    </Container>
   );
 }
 
